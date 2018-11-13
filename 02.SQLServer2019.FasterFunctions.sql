97a992c3533127aa8b5b30c502c1ecbfb398caf7
Andrea Benedetti <anbened@live.com>
2018-11-13 15:09:20 +0100

    add notes

--- 02.SQLServer2019.FasterFunctions.sql
+++ 02.SQLServer2019.FasterFunctions.sql
@@ -1,5 +1,8 @@
 
 /*
+--> DATABASE [StackOverflow2010]: https://www.brentozar.com/archive/2015/10/how-to-download-the-stack-overflow-database-via-bittorrent/
+
+
 --> Include Actual Execution Plan <--
 
 --> Compatibility SQL Server 2017 <--
